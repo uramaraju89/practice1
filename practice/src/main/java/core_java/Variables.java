abf6904a485f480ed40c3501b8dd85a82ee83d84
unknown <ramarajuuppalapati89@gmail.com>
2025-05-24 14:54:20 +0530

    git add files

--- practice/src/main/java/core_java/Variables.java
+++ practice/src/main/java/core_java/Variables.java
@@ -5,10 +5,17 @@ public class Variables {
         int a = 20;
         long v = 30l;
         sumOfTwo(10,919);
+        Variables obj = new Variables();
+        obj.valueOfTwo("this needs to be typed in caps");
 
 
     }
     public static void sumOfTwo(int a, int b){
         System.out.println(a+b  +"you sum of two values");
+    }
+
+    public void valueOfTwo(String s ){
+        System.out.println(s.toUpperCase());
+
     }
 }
